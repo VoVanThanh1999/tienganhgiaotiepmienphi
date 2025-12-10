64e8211c9beb5173fe892ea77516eeea21706ad0
ThanhVV <thanhvv@ThanhVVs-Laptop.local>
2025-12-10 15:52:13 +0700

    ini project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# tienganhgiaotiepmienphi
+ l# tienganhgiaotiepmienphi
 Dự án tiếng anh giao tiếp miễn phí
+
+Angular tham khao tai link ben duoi
+https://github.com/borjapazr/angular-skeleton
---
+++ backend/english-speak-free-auth/src/main/java/com/btc/english_speak_free_auth/EnglishSpeakFreeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.btc.english_speak_free_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishSpeakFreeAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishSpeakFreeAuthApplication.class, args);
+	}
+
+}
